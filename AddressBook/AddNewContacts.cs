58c87c69137e402da59aa769d354778ea4b0afc1
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-29 21:03:59 +0530

    [Shivani] UC2 - Add new contact in address book

--- AddressBook/AddNewContacts.cs
+++ AddressBook/AddNewContacts.cs
@@ -8,24 +8,9 @@ namespace AddressBook
 {
     public class AddNewContacts
     {
-        public static void AddNewcontactsInAddressBook()
+        public static void AddNewcontactsInAddressBook(List<Contacts> list)
         {
-            Contacts user1 = new Contacts("Shivani", "Patidar", "Patpala", "Ujjain", "Madhya Pradesh", "456060", "8897798662", "shivani@gmail.com");
-            Contacts user2 = new Contacts("Arpita", "Patidar", "Maksi", "Ujjain", "Madhya Pradesh", "456160", "9023254862", "arpita@gmail.com");
-            Contacts user3 = new Contacts("Ronak", "Sharma", "Ujjain", "Ujjain", "Madhya Pradesh", "456010", "6264589721", "ronak@gmail.com");
-            Contacts user4 = new Contacts("Roanak", "Patil", "Mhow", "Indore", "Madhya Pradesh", "456611", "7788256341", "abhishek@gmail.com");
-            Contacts user5 = new Contacts("Manoj", "Tiwari", "Rau", "Indore", "Madhya Pradesh", "4566612", "8090263548", "manoj@gmail.com");
-
-            LinkedList<Contacts> list = new LinkedList<Contacts>();
-            list.AddLast(user1);
-            list.AddLast(user2);
-            list.AddLast(user3);
-            list.AddLast(user4);
-            list.AddLast(user5);
-            foreach (Contacts contact in list)
-            {
-                contact.Show();
-            }
+            
 
             Console.WriteLine("Add a new contacts in adderss book");
             Console.WriteLine("Enter a First Name");
@@ -44,9 +29,9 @@ namespace AddressBook
             string phoneNumber = Console.ReadLine();
             Console.WriteLine("Enter a email");
             string email = Console.ReadLine();
-            Contacts user6=new Contacts(firstName,lastName,address,city,state,zip,phoneNumber,email);
+            Contacts user=new Contacts(firstName,lastName,address,city,state,zip,phoneNumber,email);
             
-            list.AddLast(user6);
+            list.Add(user);
             foreach (Contacts contact in list)
             {
                 contact.Show();
