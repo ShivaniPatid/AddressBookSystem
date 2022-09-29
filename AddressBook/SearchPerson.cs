505ddac8a5523e4e814f09fcf06a949567c5bdac
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-29 20:46:20 +0530

    [Shivani] UC8 - Search person in a city or state

---
+++ AddressBook/SearchPerson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class SearchPerson
+    {
+        public static void SearchPersonInAddressBook(List<Contacts> list)
+        {
+
+            foreach (var contact in list.FindAll(e=>(e.City=="Indore" && e.State== "Madhya Pradesh")).ToList())
+            {
+                //contact.Show();
+                
+                Console.WriteLine(contact.FirstName);
+            }
+        }
+    }
+}
