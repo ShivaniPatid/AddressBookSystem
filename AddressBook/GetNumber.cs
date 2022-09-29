666145c8bac95c4fc827a04b61260080f498bae0
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-29 20:51:59 +0530

    [Shivani] UC10 - Get number of contact persons

---
+++ AddressBook/GetNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class GetNumber
+    {
+        public static void GetNumberInAddressBook(List<Contacts> list)
+        {
+            foreach (var contact in list.FindAll(e => (e.City == "Indore" && e.State == "Madhya Pradesh")).ToList())
+            {
+                //contact.Show();
+
+                Console.WriteLine("Name : "+contact.FirstName+"\t"+"Number : "+contact.PhoneNumber);
+            }
+        }
+    }
+}
