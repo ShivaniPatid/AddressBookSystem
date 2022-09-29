6471714839a687042c4fc67dd63d8c8be88e4837
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-29 21:02:27 +0530

    [Shivani] UC1 - create a contact in address book

--- AddressBook/CreateContacts.cs
+++ AddressBook/CreateContacts.cs
@@ -9,20 +9,20 @@ namespace AddressBook
 {
     public class CreateContacts
     {
-        public static void CreateContactsInAddressBook()
+        public static void CreateContactsInAddressBook(List<Contacts> list)
         {
-            Contacts user1 = new Contacts("Shivani", "Patidar", "Patpala", "Ujjain", "Madhya Pradesh", "456060", "8897798662", "shivani@gmail.com");
-            Contacts user2 = new Contacts("Arpita", "Patidar", "Maksi", "Ujjain", "Madhya Pradesh", "456160", "9023254862", "arpita@gmail.com");
-            Contacts user3 = new Contacts("Ronak", "Sharma", "Ujjain", "Ujjain", "Madhya Pradesh", "456010", "6264589721", "ronak@gmail.com");
-            Contacts user4 = new Contacts("Roanak", "Patil", "Mhow", "Indore", "Madhya Pradesh", "456611", "7788256341", "abhishek@gmail.com");
-            Contacts user5 = new Contacts("Manoj", "Tiwari", "Rau", "Indore", "Madhya Pradesh", "4566612", "8090263548", "manoj@gmail.com");
+            Contacts user1 = new Contacts("Shivani", "Patidar", "11 Street", "Ujjain", "Madhya Pradesh", "456060", "8897798662", "shivani@gmail.com");
+            Contacts user2 = new Contacts("Arpita", "Patidar", "12 Street", "Ujjain", "Madhya Pradesh", "456160", "9023254862", "arpita@gmail.com");
+            Contacts user3 = new Contacts("Ronak", "Sharma", "13 Street", "Ujjain", "Madhya Pradesh", "456010", "6264589721", "ronak@gmail.com");
+            Contacts user4 = new Contacts("Roanak", "Patil", "14 Street", "Indore", "Madhya Pradesh", "456611", "7788256341", "abhishek@gmail.com");
+            Contacts user5 = new Contacts("Manoj", "Tiwari", "15 Street", "Indore", "Madhya Pradesh", "4566612", "8090263548", "manoj@gmail.com");
             
-            LinkedList<Contacts> list = new LinkedList<Contacts>();
-            list.AddLast(user1);
-            list.AddLast(user2);
-            list.AddLast(user3);
-            list.AddLast(user4);
-            list.AddLast(user5);
+            
+            list.Add(user1);
+            list.Add(user2);
+            list.Add(user3);
+            list.Add(user4);
+            list.Add(user5);
             foreach (Contacts contact in list)
             {
                 contact.Show();
