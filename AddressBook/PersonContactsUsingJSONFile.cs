c2b4ac85c67728f9c1ec3c167091a03c5bf59524
ShivaniPatid <shivanipatidar712@gmail.com>
2022-10-01 21:35:02 +0530

    [Shivani] UC15 - Read/Write the address book with person contact as JSON File

---
+++ AddressBook/PersonContactsUsingJSONFile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using Newtonsoft.Json;
+
+namespace AddressBook
+{
+    public class PersonContactsUsingJSONFile
+    {
+        public static void Serialize(List<Contacts> list)
+        {
+            string csvFilePath = @"D:\visualstudio\AddressBookSystem\AddressBook\CsvPersonData.txt";
+
+            StreamWriter streamWriter = new StreamWriter(csvFilePath);
+            CsvWriter csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture);
+
+            csvWriter.WriteRecords<Contacts>(list);
+            streamWriter.Close();
+            Console.WriteLine("Closed");
+        }
+
+        public static void Deserialize()
+        {
+            string csvFilePath = @"D:\visualstudio\AddressBookSystem\AddressBook\CsvPersonData.txt";
+
+            string jsonFilePath= @"D:\visualstudio\AddressBookSystem\AddressBook\jsonPersonData.txt";
+
+            StreamReader streamReader = new StreamReader(csvFilePath);
+            CsvReader csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+            List<Contacts> contacts = csvReader.GetRecords<Contacts>().ToList();
+            foreach (Contacts contact in contacts)
+            {
+                contact.Show();
+            }
+
+            JsonSerializer jsonSerializer = new JsonSerializer();
+            using(StreamWriter stream=new StreamWriter(jsonFilePath))
+                using(JsonWriter jsonWriter = new JsonTextWriter(stream))
+                {
+                    jsonSerializer.Serialize(jsonWriter, contacts);
+                }
+        }
+    }
+}
